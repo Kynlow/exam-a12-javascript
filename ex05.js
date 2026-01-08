baa87fd5626cb7044d0d50615619a42f41a620eb
Kynlow <elwen@areastudio.fr>
2026-01-08 11:17:07 +0100

    ex05

--- ex05.js
+++ ex05.js
@@ -1,17 +1,18 @@
 function compterVoyelles(chaine) {
-    let compteur = 0
-    for (let i = 0; i < chaine.length; i++) {
-        if (chaine[i] == "a" ||
-            chaine[i] == "e" || 
-            chaine[i] == "i" || 
-            chaine[i] == "o" || 
-            chaine[i] == "u" || 
-            chaine[i] == "y" 
-         ) {
-            compteur++
-         }
-    }
-    return compteur
+	let compteur = 0;
+	for (let i = 0; i <= chaine.length; i++) {
+		if (
+			chaine[i] == "a" ||
+			chaine[i] == "e" ||
+			chaine[i] == "i" ||
+			chaine[i] == "o" ||
+			chaine[i] == "u" ||
+			chaine[i] == "y"
+		) {
+			compteur++;
+		}
+	}
+	return compteur;
 }
 
 console.log(compterVoyelles("bonjour")); // Résultat attendu : 3
